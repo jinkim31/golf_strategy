4e8c1a858fd0d49d6c8247e31ad509d52f20decb
jinkim31 <jinkim5771@gmail.com>
2022-05-30 16:54:15 +0900

    added angle,club publishers

--- src/node.py
+++ src/node.py
@@ -1,6 +1,7 @@
 import rospy
 import geometry_msgs.msg
 import sensor_msgs.msg
+import std_msgs.msg
 from golf_env.src import golf_env
 from golf_env.src import heuristic_agent
 from cv_bridge import CvBridge
@@ -11,11 +12,14 @@ class Node(object):
     def __init__(self):
         rospy.init_node('golf_strategy')
 
-        self._img_pub = rospy.Publisher('img', sensor_msgs.msg.Image, queue_size=10)
-        self._point_sub = rospy.Subscriber('point', geometry_msgs.msg.Point, self._point_callback)
+        self._stroke_angles_pub = rospy.Publisher('golf_stroke_angles', std_msgs.msg.Float64MultiArray, queue_size=1)
+        self._club_indexes_pub = rospy.Publisher('golf_club_indexes', std_msgs.msg.Int8MultiArray, queue_size=1)
+        self._club_name_pub = rospy.Publisher('golf_club_names', std_msgs.msg.String, queue_size=1)
+        self._img_pub = rospy.Publisher('golf_img', sensor_msgs.msg.Image, queue_size=1)
+        self._point_sub = rospy.Subscriber('golf_point', geometry_msgs.msg.Point, self._point_callback)
         self._count = 0
         self.bridge = CvBridge()
-        self.env = golf_env.GolfEnv('sophia_green')
+        self.env = golf_env.GolfEnv('straight')
         self.agent = heuristic_agent.HeuristicAgent()
         self.state = self.env.reset()
 
@@ -28,12 +32,32 @@ class Node(object):
 
     def _point_callback(self, msg):
         self.state = self.env.reset(initial_pos=[msg.x, msg.y])
+        episode_img, stroke_angles, club_indexes, club_names = self._generate_episode()
+        img_msg = self.bridge.cv2_to_imgmsg(episode_img, encoding='passthrough')
+
+        self._img_pub.publish(img_msg)
+        self._stroke_angles_pub.publish(std_msgs.msg.Float64MultiArray(data=stroke_angles))
+        self._club_indexes_pub.publish(std_msgs.msg.Int8MultiArray(data=club_indexes))
+        self._club_name_pub.publish(' '.join(club_names))
+
+
+    def _generate_episode(self):
+        stroke_angles = []
+        club_names = []
+        club_indexes = []
 
         while True:
-            self.state, reward, termination = self.env.step(self.agent.step(self.state))
+            action = self.agent.step(self.state)
+
+            stroke_angles.append(action[0])
+            club_indexes.append(action[1])
+            club_names.append(golf_env.GolfEnv.CLUB_INFO[action[1]][golf_env.GolfEnv.ClubInfoIndex.NAME])
+
+            self.state, reward, termination = self.env.step(action)
+
             if termination:
                 break
 
-        img = self.env.paint()
-        msg = self.bridge.cv2_to_imgmsg(img, encoding='passthrough')
-        self._img_pub.publish(msg)
+        episode_img = self.env.paint()
+
+        return episode_img, stroke_angles, club_indexes, club_names
