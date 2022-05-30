83bbfbfbb05c9533bb9f118e04c63c2fe22008e9
jinkim31 <jinkim5771@gmail.com>
2022-05-30 15:28:30 +0900

    updated golf_env

--- src/node.py
+++ src/node.py
@@ -12,7 +12,7 @@ class Node(object):
         rospy.init_node('golf_strategy')
 
         self._img_pub = rospy.Publisher('img', sensor_msgs.msg.Image, queue_size=10)
-        self._point_sub = rospy.Subscriber('foo', geometry_msgs.msg.Point, self._point_callback)
+        self._point_sub = rospy.Subscriber('point', geometry_msgs.msg.Point, self._point_callback)
         self._count = 0
         self.bridge = CvBridge()
         self.env = golf_env.GolfEnv('sophia_green')
@@ -27,8 +27,13 @@ class Node(object):
         pass
 
     def _point_callback(self, msg):
-        self.env.reset(initial_pos=[msg.x, msg.y])
-        self.state, reward, termination = self.env.step(self.agent.step(self.state))
+        self.state = self.env.reset(initial_pos=[msg.x, msg.y])
+
+        while True:
+            self.state, reward, termination = self.env.step(self.agent.step(self.state))
+            if termination:
+                break
+
         img = self.env.paint()
         msg = self.bridge.cv2_to_imgmsg(img, encoding='passthrough')
         self._img_pub.publish(msg)
