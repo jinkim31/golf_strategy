60ed77971d752dafa47eb43c15fea0852f527ddb
jinkim31 <jinkim5771@gmail.com>
2022-05-30 14:33:25 +0900

    README.md

--- src/node.py
+++ src/node.py
@@ -1,21 +1,23 @@
 import rospy
 from std_msgs.msg import String
+import sensor_msgs.msg
 from golf_env.src import golf_env
 from golf_env.src import heuristic_agent
-
+from cv_bridge import CvBridge
 
 class Node(object):
 
     def __init__(self):
-        self._pub = rospy.Publisher('foo', String, queue_size=10)
+        self._img_pub = rospy.Publisher('img', sensor_msgs.msg.Image, queue_size=10)
         self._sub = rospy.Subscriber('foo', String, self.__callback)
         self._count = 0
+        self.bridge = CvBridge()
 
-        self.env = golf_env.GolfEnv('straight')
+        self.env = golf_env.GolfEnv('sophia_green')
         self.agent = heuristic_agent.HeuristicAgent()
         self.state = self.env.reset()
 
-        rospy.init_node('boilerplate')
+        rospy.init_node('golf_strategy')
 
         while not rospy.is_shutdown():
             self.__spin_once()
@@ -23,9 +25,9 @@ class Node(object):
 
     def __spin_once(self):
         self.state, reward, termination = self.env.step(self.agent.step(self.state))
-        self.env.plot()
-        self._count += 1
-        self._pub.publish('Hello world #{}'.format(self._count))
+        img = self.env.paint()
+        msg = self.bridge.cv2_to_imgmsg(img, encoding='passthrough')
+        self._img_pub.publish(msg)
 
     def __callback(self, msg):
         rospy.loginfo('Received {}'.format(msg.data))
